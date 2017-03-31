8b7afed6cdea45139a955f7cdaef317193873ec1
LEE ZHE YU <tzef8220@gmail.com>
2017-03-31 18:01:56 +0800

    Initial commit

---
+++ Example/BmoViewPager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BmoViewPager
+//
+//  Created by LEE ZHE YU on 03/31/2017.
+//  Copyright (c) 2017 LEE ZHE YU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
